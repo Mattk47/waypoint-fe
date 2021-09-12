abe1ae063627184cf73c86728060d52cf5fab39e
thick-hollins <alexnuttall@zoho.eu>
2021-09-12 14:41:24 +0100

    initial

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location'
+import * as TaskManager from 'expo-task-manager'
+
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+    })()
+  }, [])
+
+  const startTracking = async () => {
+    await Location.startLocationUpdatesAsync('bgLocation', {
+      accuracy: Location.Accuracy.Lowest,
+      timeInterval: 1000,
+      distanceInterval: 0,
+      foregroundService: {
+        notificationTitle: 'Tracking is active',
+        notificationBody: 'REcording your route',
+        notificationColor: '#333333',
+      }
+    });
+    console.log('[tracking]', 'started background location task');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='start tracking' onPress={startTracking}/>
     </View>
   );
 }
@@ -18,4 +46,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+TaskManager.defineTask('bgLocation', async (event) => {
+  if (event.error) {
+    return console.error('[tracking]', 'Something went wrong within the background location task...', event.error);
+  }
+
+  console.log(event.data)
 });
