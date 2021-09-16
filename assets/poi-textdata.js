cfe1d736a4c612e09baf2c04225c4673c23fcef5
Christopher Pearson <cjpearson@hotmail.com>
2021-09-16 16:36:28 +0100

    add test waypoint markers to post screen

---
+++ assets/poi-textdata.js
@@ -0,0 +1,7 @@
+export default [
+  { latitude: 53.841475, longitude: -1.781697 },
+  { latitude: 53.844275, longitude: -1.777048 },
+  { latitude: 53.849231, longitude: -1.767623 },
+  { latitude: 53.843763, longitude: -1.779492 },
+  { latitude: 53.840532, longitude: -1.781661 },
+]
