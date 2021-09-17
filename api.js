14fc0bb9b4770a9b83b79c7248a9dd55a2a7a0d8
Christopher Pearson <cjpearson@hotmail.com>
2021-09-17 18:04:06 +0100

    fix axios requests

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 })
 
 export const getUser = async (username) => {
-  const { data } = await api.get(`/users/:${username}`)
+  const { data } = await api.get(`/users/${username}`)
   return data
 }
 
@@ -15,12 +15,12 @@ export const getUsers = async () => {
 }
 
 export const postUser = async (userObj) => {
-  const { data } = await axios.post('/users', userObj)
+  const { data } = await api.post('/users', userObj)
   return data
 }
 
 export const patchUser = async (name, avatar_url, password, username) => {
-  const { data } = await axios.patch(`/users/:${username}`, {
+  const { data } = await api.patch(`/users/${username}`, {
     name,
     avatar_url,
     password,
@@ -29,47 +29,47 @@ export const patchUser = async (name, avatar_url, password, username) => {
 }
 
 export const deleteUser = async (username) => {
-  const { data } = await axios.delete(`/users/:${username}`)
+  const { data } = await api.delete(`/users/${username}`)
   return data
 }
 
 export const getRouteByUser = async (username) => {
-  const { data } = await axios.get(`/routes/:${username}`)
+  const { data } = await api.get(`/routes/${username}`)
   return data
 }
 
 export const getAllRoutes = async () => {
-  const { data } = await axios.get('/routes')
+  const { data } = await api.get('/routes')
   return data
 }
 
 export const getRouteById = async (routeId) => {
-  const { data } = await axios.get(`/routes/:${routeId}`)
+  const { data } = await api.get(`/routes/${routeId}`)
   return data
 }
 
 export const postRoute = async (routeObj) => {
-  const { data } = await axios.post('/routes', routeObj)
+  const { data } = await api.post('/routes', routeObj)
   return data
 }
 
 export const patchRouteById = async (routeId, vote) => {
-  const { data } = await axios.patch(`/routes/:${routeId}`, { vote })
+  const { data } = await api.patch(`/routes/${routeId}`, { vote })
   return data
 }
 
 export const deleteRouteById = async (routeId) => {
-  const { data } = await axios.delete(`/routes/:${routeId}`)
+  const { data } = await api.delete(`/routes/${routeId}`)
   return data
 }
 
 export const getPoiByRouteById = async (routeId) => {
-  const { data } = await axios.get(`/routes/:${routeId}/poi`)
+  const { data } = await api.get(`/routes/${routeId}/poi`)
   return data
 }
 
 export const postPoiByRouteById = async (routeId, photo, narration) => {
-  const { data } = await axios.post(`/routes/:${routeId}/poi`, {
+  const { data } = await api.post(`/routes/${routeId}/poi`, {
     photo,
     narration,
   })
@@ -77,30 +77,30 @@ export const postPoiByRouteById = async (routeId, photo, narration) => {
 }
 
 export const patchPoi = async (poiId, narration) => {
-  const { data } = await axios.patch(`/poi/:${poiId}`, { narration })
+  const { data } = await api.patch(`/poi/${poiId}`, { narration })
   return data
 }
 
 export const deletePoi = async (poiId) => {
-  const { data } = await axios.delete(`/poi/:${poiId}`)
+  const { data } = await api.delete(`/poi/${poiId}`)
   return data
 }
 export const getCommentsByRoute = async (routeId) => {
-  const { data } = await axios.get(`/routes/:${routeId}/comments`)
+  const { data } = await api.get(`/routes/${routeId}/comments`)
   return data
 }
 
 export const postCommentsByRoute = async (routeId, commentObj) => {
-  const { data } = await axios.post(`/routes/:${routeId}/comments`, commentObj)
+  const { data } = await api.post(`/routes/${routeId}/comments`, commentObj)
   return data
 }
 
 export const deleteComment = async (commentId) => {
-  const { data } = await axios.delete(`/comments/${commentId}`)
+  const { data } = await api.delete(`/comments/${commentId}`)
   return data
 }
 
 export const patchComment = async (commentId, vote) => {
-  const { data } = await axios.patch(`/comments/${commentId}`, { vote })
+  const { data } = await api.patch(`/comments/${commentId}`, { vote })
   return data
 }
