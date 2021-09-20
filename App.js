f00965b16fdff79ae96f0df896f66e48e2495248
mr-joelM <meyerjoel01@gmail.com>
2021-09-20 11:29:24 +0100

    icons in nav bar

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react'
+import React /*useState*/ from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Record from './src/Screens/Record'
 import FeedNavigator from './src/Navigators/FeedNavigator'
 import ProfileNavigator from './src/Navigators/ProfileNavigator'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 // import LoggedOutNavigator from './src/Navigators/LoggedOutNavigator'
 // import testUser from './dummyUser'
 
 const Tab = createBottomTabNavigator()
 
 export const AppNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName
+
+        if (route.name === 'Home') {
+          iconName = focused ? 'home-circle' : 'home-circle-outline'
+        } else if (route.name === 'Record') {
+          iconName = focused ? 'record-circle' : 'record-circle-outline'
+        } else if (route.name === 'You') {
+          iconName = focused ? 'account-circle' : 'account-circle-outline'
+        }
+
+        return (
+          <MaterialCommunityIcons name={iconName} size={size} color={color} />
+        )
+      },
+      tabBarActiveTintColor: 'blue',
+      tabBarInactiveTintColor: 'gray',
+    })}
+  >
     <Tab.Screen
       name="Home"
       component={FeedNavigator}
