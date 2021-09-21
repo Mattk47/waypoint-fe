480772d4e1b792bd10de0648143fc16a07bbac83
Christopher Pearson <cjpearson@hotmail.com>
2021-09-21 22:19:24 +0100

    independant userRoute state & styling

--- src/Screens/Profile.jsx
+++ src/Screens/Profile.jsx
@@ -6,16 +6,23 @@ import {
   Dimensions,
   Image,
   Pressable,
+  FlatList,
+  ScrollView,
 } from 'react-native'
-import { getUser } from '../../api'
+import { getAllRoutes, getUser } from '../../api'
 import { AppUserContext } from '../../contexts'
+import FeedCard from '../Components/FeedCard'
 import Feed from './Feed'
 
 export default Profile = ({ route, navigation }) => {
   const { appUser } = useContext(AppUserContext)
   const { user_id } = route.params || appUser
+
   const [user, setUser] = useState({})
+  const [userRoutes, setUserRoutes] = useState([])
   const [postCount, setPostCount] = useState(0)
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
 
   useEffect(() => {
     getUser(user_id)
@@ -26,71 +33,93 @@ export default Profile = ({ route, navigation }) => {
       .catch((err) => {
         console.log(err)
       })
-  }, [])
+    getAllRoutes(page, user_id)
+      .then(({ routes, totalPages, totalResults }) => {
+        setTotalPages(totalPages)
+        setPostCount(totalResults)
+        setUserRoutes((curr) => {
+          return [...new Set([...curr, ...routes])]
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [page])
 
   const { name, bio, avatar_url } = user
   const follow = Math.ceil(Math.random() * 500)
   const max = Math.floor(follow * 1.1)
   const min = Math.ceil(follow * 0.9)
 
+  const renderRoute = (route) => <FeedCard route={route} />
+
   return (
-    <>
-      <View style={ProfileStyles.card}>
-        <View style={ProfileStyles.rowOne}>
-          <Image
-            style={ProfileStyles.avatar}
-            source={{
-              uri: avatar_url,
-            }}
-            resizeMode="cover"
-          />
-          <View style={ProfileStyles.content}>
-            <Text style={ProfileStyles.text}>{name}</Text>
-            <View style={ProfileStyles.counters}>
-              <Pressable>
-                <View style={ProfileStyles.counter}>
-                  <Text style={ProfileStyles.label}>Posts</Text>
-                  <Text style={ProfileStyles.numberOf}>{postCount}</Text>
-                </View>
-              </Pressable>
-              <Pressable onPress={() => navigation.navigate('Followers')}>
-                <View style={ProfileStyles.counter}>
-                  <Text style={ProfileStyles.label}>Followers</Text>
-                  <Text style={ProfileStyles.numberOf}>{follow}</Text>
-                </View>
-              </Pressable>
-              <Pressable onPress={() => navigation.navigate('Following')}>
-                <View style={[ProfileStyles.counter, { borderRightWidth: 0 }]}>
-                  <Text style={ProfileStyles.label}>Following</Text>
-                  <Text style={ProfileStyles.numberOf}>
-                    {Math.floor(Math.random() * (max - min + 1) + min)}
-                  </Text>
-                </View>
-              </Pressable>
-            </View>
+    <ScrollView style={ProfileStyles.card}>
+      <View style={ProfileStyles.rowOne}>
+        <Image
+          style={ProfileStyles.avatar}
+          source={{
+            uri: avatar_url,
+          }}
+          resizeMode="cover"
+        />
+        <View style={ProfileStyles.content}>
+          <Text style={ProfileStyles.text}>{name}</Text>
+          <View style={ProfileStyles.counters}>
+            <Pressable>
+              <View style={ProfileStyles.counter}>
+                <Text style={ProfileStyles.label}>Posts</Text>
+                <Text style={ProfileStyles.numberOf}>{postCount}</Text>
+              </View>
+            </Pressable>
+            <Pressable onPress={() => navigation.navigate('Followers')}>
+              <View style={ProfileStyles.counter}>
+                <Text style={ProfileStyles.label}>Followers</Text>
+                <Text style={ProfileStyles.numberOf}>{follow}</Text>
+              </View>
+            </Pressable>
+            <Pressable onPress={() => navigation.navigate('Following')}>
+              <View style={[ProfileStyles.counter, { borderRightWidth: 0 }]}>
+                <Text style={ProfileStyles.label}>Following</Text>
+                <Text style={ProfileStyles.numberOf}>
+                  {Math.floor(Math.random() * (max - min + 1) + min)}
+                </Text>
+              </View>
+            </Pressable>
           </View>
         </View>
+      </View>
+      <View style={{ borderBottomWidth: 1, borderColor: 'darkgrey' }}>
         <Text style={ProfileStyles.bio}>{bio}</Text>
       </View>
-      <Feed user_id={user_id} hideName={true} setPostCount={setPostCount} />
-    </>
+      <FlatList
+        data={userRoutes}
+        renderItem={renderRoute}
+        keyExtractor={(route) => route._id}
+        onEndReached={() => {
+          if (page < totalPages) setPage((curr) => curr + 1)
+        }}
+        onEndReachedThreshold={2}
+      />
+    </ScrollView>
   )
 }
 
 const ProfileStyles = StyleSheet.create({
   card: {
     width: Dimensions.get('window').width,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     borderBottomWidth: 1,
     borderColor: 'darkgrey',
     // borderTopWidth: 1,
-    padding: 15,
+    // padding: 15,
     // marginBottom: 10,
   },
   rowOne: {
+    backgroundColor: '#fff',
     flexDirection: 'row',
-    marginBottom: 10,
-    paddingBottom: 15,
+    // marginBottom: 10,
+    padding: 15,
     borderBottomWidth: 1,
     borderColor: 'darkgrey',
   },
@@ -126,5 +155,11 @@ const ProfileStyles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     fontWeight: 'bold',
+  },
+  bio: {
+    padding: 15,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderColor: 'darkgrey',
   },
 })
