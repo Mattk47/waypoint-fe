9ff5e69615cdd4115b733a2016901be570e2eea6
Christopher Pearson <cjpearson@hotmail.com>
2021-09-23 11:23:20 +0100

    create custom styled button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import { Button } from "react-native";
+import { Pressable, Text, View } from 'react-native'
 
-
-const DefaultButton = ({ buttonText }) => {
-
-
-    return (
-        <Button
-            title={buttonText}
-            onPress={() => {
-                setLiked(false);
-                setLikes(curr => curr - 1);
-            }}
+const DefaultButton = ({
+  buttonText,
+  disabled,
+  onPressFunc,
+  styleOverride,
+}) => {
+  return (
+    <View
+      style={{
+        paddingTop: 15,
+        paddingBottom: 15,
+        flex: 1,
+        justifyContents: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Pressable
+        style={{
+          borderRadius: 20,
+          padding: 10,
+          elevation: 2,
+          backgroundColor: 'rgb(70, 184, 159)',
+          width: 100,
+        }}
+        onPress={onPressFunc}
+        disabled={disabled}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
         >
-
-        </Button>
-    )
-};
+          {buttonText}
+        </Text>
+      </Pressable>
+    </View>
+  )
+}
 
 export default DefaultButton;
